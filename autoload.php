e78fb251f4a76b6d1ca61b521f544297a6f087b4
enikeishik <enikeishik@gmail.com>
2018-08-18 12:41:58 +0300

    Changed autoloader

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 spl_autoload_register(
     function ($class) {
         //BOOKMARK: use DIRECTORY_SEPARATOR instead of `/` on non *nix
-        $class = str_replace('Ufocms\\', 'ufocms/', $class);
+        $class = str_replace('CITest\\', 'citest/', $class);
         $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
         if (file_exists($file)) {
             require $file;
