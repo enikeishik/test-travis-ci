b4b75eef9b221c5d51112f4329eab122de57255a
enikeishik <enikeishik@gmail.com>
2018-08-18 12:47:41 +0300

    Added some test case

--- test.php
+++ test.php
@@ -6,5 +6,8 @@ date_default_timezone_set('Europe/Moscow');
 require_once 'autoload.php';
 
 $loader = new \CITest\Frontend\Loader();
+$loader->setUrl('http://informer.gismeteo.ru/xml/27612.xml');
+$data = $loader->getData();
+assert($data);
 
 echo 'OK';
