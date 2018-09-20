f095de42bc3411a0d81fb58cac849dae34824e5f
enikeishik <enikeishik@gmail.com>
2018-09-20 10:58:14 +0300

    fix: paths and dbname

--- tests/config-cctests.php
+++ tests/config-cctests.php
@@ -11,7 +11,7 @@ define('C_CACHE', false);
 define('C_DB_SERVER', 'localhost');
 define('C_DB_USER', 'root');
 define('C_DB_PASSWD', '');
-define('C_DB_NAME', 'cctestsdb');
+define('C_DB_NAME', 'ttci');
 define('C_DB_TABLE_PREFIX', '');
 define('C_DB_CHARSET', 'utf8');
 
