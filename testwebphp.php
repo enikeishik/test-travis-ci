bfa3920780df643db2c76b482e04838b0a8822f0
enikeishik <enikeishik@gmail.com>
2018-09-26 13:02:01 +0300

    chore: removed all PHP directives from web PHP script

--- testwebphp.php
+++ testwebphp.php
@@ -1,9 +1,9 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('error_reporting', E_ALL);
-ini_set('scream.enabled', true);
-date_default_timezone_set('Europe/Moscow');
-setlocale(LC_ALL, 'ru_RU.utf-8', 'rus_RUS.utf-8', 'ru_RU.utf8');
-mb_internal_encoding('UTF-8');
+// ini_set('display_errors', '1');
+// ini_set('error_reporting', E_ALL);
+// ini_set('scream.enabled', true);
+// date_default_timezone_set('Europe/Moscow');
+// setlocale(LC_ALL, 'ru_RU.utf-8', 'rus_RUS.utf-8', 'ru_RU.utf8');
+// mb_internal_encoding('UTF-8');
 
 echo 'test web PHP content';
