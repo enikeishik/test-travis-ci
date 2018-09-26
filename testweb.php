06666af4fc0c5bea35ef0951f0d4c22a1b0bc29a
enikeishik <enikeishik@gmail.com>
2018-09-26 08:20:47 +0300

    feat: make test web

--- testweb.php
+++ testweb.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', '1');
 ini_set('error_reporting', E_ALL);
 date_default_timezone_set('Europe/Moscow');
+assert_options(ASSERT_ACTIVE, true);
 
 define('CONTENT', 'test web content');
 
