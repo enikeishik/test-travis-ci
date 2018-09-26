18d4e742668690faa2621d1a622dc8a1eea17dd5
enikeishik <enikeishik@gmail.com>
2018-09-26 12:36:04 +0300

    feat: added web PHP script and test for it

--- testweb.php
+++ testweb.php
@@ -5,6 +5,10 @@ date_default_timezone_set('Europe/Moscow');
 assert_options(ASSERT_ACTIVE, true);
 
 define('CONTENT', 'test web content');
+define('PHP_CONTENT', 'test web PHP content');
 
 $content = file_get_contents('http://localhost/testweb.html');
 assert(false !== strpos($content, CONTENT));
+
+$content = file_get_contents('http://localhost/testwebphp.php');
+assert(false !== strpos($content, PHP_CONTENT));
