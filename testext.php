13551ce7e301a7edc864fe5e1a9b6c3cc7e83fb6
enikeishik <enikeishik@gmail.com>
2019-02-23 10:48:06 +0300

    feat: added checks for class existings

--- testext.php
+++ testext.php
@@ -3,9 +3,18 @@ ini_set('display_errors', '1');
 ini_set('error_reporting', E_ALL);
 date_default_timezone_set('Europe/Moscow');
 
-$m = new Memcached();
-assert(Memcached::class === get_class($m));
-$m = new Memcache();
-assert(Memcache::class === get_class($m));
+if (class_exists('Memcached')) {
+    $m = new Memcached();
+    assert(Memcached::class === get_class($m));
+} else {
+    echo 'class Memcached not exists' . PHP_EOL;
+}
+
+if (class_exists('Memcache')) {
+    $m = new Memcache();
+    assert(Memcache::class === get_class($m));
+} else {
+    echo 'class Memcache not exists' . PHP_EOL;
+}
 
 echo 'OK';
